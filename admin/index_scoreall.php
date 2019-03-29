fe8939ef7b012ee37f3193bfb6ebdc2bc5a5b53e
nopparat231 <23.noop@gmail.com>
2019-03-29 23:04:29 +0700

    edit sm

--- admin/index_scoreall.php
+++ admin/index_scoreall.php
@@ -1,30 +1,30 @@
+
 <?php 
 
-
-
-$query_learning = "SELECT * FROM choice as c , user as u, user_learning as l WHERE l.choice_id = c.choice_id and l.user_id = u.id order by l.user_learning_af desc" ;
+$query_learning = "SELECT * FROM choice as c , user as u, user_learning as l where l.choice_id = c.choice_id and l.user_id = u.id order by l.user_learning_af desc" ;
 $learning = mysqli_query($con,$query_learning) or die(mysqli_error());
 $row_learning = mysqli_fetch_assoc($learning);
 $totalRows_learning = mysqli_num_rows($learning);
 
 
 ?>
-
 <div class="col-md-9 bg-light">
  <div class="py-2">
   <div class="container">
     <div class="row">
       <div class="col-md-12">
-        <h1 class="text-center" >คะแนนผู้ใช้งานทั้งหมด</h1>
+        <h1 class="text-center">คะแนนผู้ใช้งานทังหมด</h1>
+        <hr>
       </div>
     </div>
   </div>
 </div>
+
 <div class="py-3">
   <div class="container">
     <div class="row">
       <div class="col-md-12">
-        <div class="table-responsive text-center">
+        
           <table class="display" id="example">
            <?php if ($totalRows_learning > 0) {?>
 
@@ -35,7 +35,8 @@ $totalRows_learning = mysqli_num_rows($learning);
                 <th scope="col">นามสกุล</th>
                 <th scope="col">บทเรียน</th>
                 <th scope="col">คะแนนก่อนเรียน</th>
-                <th scope="col" contenteditable="true">คะแนนหลังเรียน</th>
+                <th scope="col">คะแนนหลังเรียน</th>
+                <th scope="col">ลบ</th>
               </tr>
             </thead>
             <tbody>
@@ -52,30 +53,33 @@ $totalRows_learning = mysqli_num_rows($learning);
                   <td><?php echo $row_learning['choice_name']; ?></td>
                   <td><?php echo $row_learning['user_learning_bf']; ?></td>
                   <td><?php echo $row_learning['user_learning_af']; ?></td>
-                </tr>
+                  <?php if ($row_learning['user_learning_status'] <> 1 ): ?>
+                   <td> 
+                    <a href="del_index_scoreall.php?user_learning_id=<?php echo $row_learning['user_learning_id'];?>&st=1" class="btn btn-outline-danger my-2 my-sm-0" onClick="return confirm('ยืนยันการยกเลิกคะแนน');"><i class="fa fa-window-close-o text-muted fa-mg"></i></a>
+                  </td>
+                  <?php else: ?>
+                    <td> 
+                      <a href="del_index_scoreall.php?user_learning_id=<?php echo $row_learning['user_learning_id'];?>&st=0" class="btn btn-outline-info my-2 my-sm-0" onClick="return confirm('ยืนยันการใช้งานคะแนน');"><i class="fa fa fa-repeat text-muted fa-mg"></i></a>
+                    </td>
+                      <?php endif ?>
+                  </tr>
 
-                <?php 
-                $i += 1;
-              } while ($row_learning = mysqli_fetch_assoc($learning)); ?>
+                  <?php 
+                  $i += 1;
+                } while ($row_learning = mysqli_fetch_assoc($learning)); ?>
 
-            </tbody>
-          </table>
-        <?php }else {
-          echo "<h3> <br /> ยังไม่มีคะแนน </h3>";
-        }
+              </tbody>
+            </table>
+          <?php }else {
+            echo "<h3> ยังไม่มีคะแนน </h3>";
+          }
 
-        mysqli_free_result($learning);?>
+          mysqli_free_result($learning);?>
 
+       
       </div>
     </div>
   </div>
 </div>
-</div>
-<div class="py-5">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12"></div>
-    </div>
-  </div>
-</div>
+
 </div>
